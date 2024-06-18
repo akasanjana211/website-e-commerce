e2cb010d442e7f236be5a83fa1d684962a5ca7d7
akasanjana211 <akasanjana2003@gmail.com>
2024-06-18 21:45:01 +0530

    Add files via upload

---
+++ E-Commerce-main/E-Commerce-main/README.md
@@ -0,0 +1,2 @@
+# E-Commerce
+This repository contains the source code for a fully responsive e-commerce website built using HTML, CSS, and JavaScript. The website is designed to provide a seamless shopping experience across various devices, adapting to different screen sizes and orientations.
---
+++ E-Commerce-main/E-Commerce-main/app.js
@@ -0,0 +1,16 @@
+let bar = document.querySelector('#bar');
+let close = document.querySelector('#close');
+let nav = document.querySelector('#navbar');
+
+if(bar) {
+    bar.addEventListener("click" , ()=>{
+        nav.classList.add("active");
+    })
+}
+
+if(close) {
+    close.addEventListener("click" , ()=>{
+        nav.classList.remove("active");
+    })
+}
+
